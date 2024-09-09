437798d914f3d55d830ed1933a358acc22797cf6
Gautam D <gautamgautm123@gmail.com>
2024-09-09 10:17:56 +0530

    update

--- full_adder_1_bit/full_adder.v
+++ full_adder_1_bit/full_adder.v
@@ -1,22 +1,22 @@
 //Full adder using structural modeling
 module full_adder (a,b,cin,sum,carry);
 
-input a;
-input b;
-input cin;
+	input a;
+	input b;
+	input cin;
 
-output sum;
-output carry;
+	output sum;
+	output carry;
 
-wire w1,w2,w3,w4;       //Internal connections
+	wire w1,w2,w3,w4;       //Internal connections
 
-xor(w1,a,b);
-xor(sum,w1,cin);        //Sum output
+	xor(w1,a,b);
+	xor(sum,w1,cin);        //Sum output
 
-and(w2,a,b);
-and(w3,b,cin);
-and(w4,cin,a);
+	and(w2,a,b);
+	and(w3,b,cin);
+	and(w4,cin,a);
 
-or(carry,w2,w3,w4);     //carry output
+	or(carry,w2,w3,w4);     //carry output
 
 endmodule
--- multiplexer/multiplexer.v
+++ multiplexer/multiplexer.v
@@ -1,1 +1,19 @@
+module multiplexer(I0, I1, I2, I3, I4, I5, I6, I7, S0, S1, S2, O0);
 
+	input I0, I1, I2, I3, I4, I5, I6, I7, S0, S1, S2;
+	output O0;
+
+	wire W0, W1, W2, W3, W4, W5, W6, W7;
+
+	and(W0, I0, ~S0, ~S1, ~S2);
+	and(W1, I1, ~S0, ~S1, S2);
+	and(W2, I2, ~S0, S1, ~S2);
+	and(W3, I3, ~S0, S1, S2);
+	and(W4, I4, S0, ~S1, ~S2);
+	and(W5, I5, S0, ~S1, S2);
+	and(W6, I6, S0, S1, ~S2);
+	and(W7, I7, S0, S1, S2);
+	
+	or(O0, W0, W1, W2, W3, W4, W5, W6, W7);
+
+endmodule
