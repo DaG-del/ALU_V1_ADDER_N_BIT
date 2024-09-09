2927c3ff43367e2272ed1e0033e5064cce353965
Gautam D <gautamgautm123@gmail.com>
2024-09-09 18:43:17 +0530

    update

---
+++ n_bit_adder.v
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ns
+
+`include "incrementer/incrementer.v"
+`include "comparator/comparator.v"
+`include "clock/clock.v"
+`include "full_adder_1_bit/full_adder.v"
+`include "multiplexer/multiplexer.v"
+
+module n_bit_adder (N2, N1, N0, )
